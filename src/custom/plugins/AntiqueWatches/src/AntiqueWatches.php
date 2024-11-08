ca0deddf213aa5d9fc8391856bd8d0fe5510df12
Binh Tran <technobia@gmail.com>
2024-11-07 21:27:42 -0600

    add theme

---
+++ src/custom/plugins/AntiqueWatches/src/AntiqueWatches.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace AntiqueWatches;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class AntiqueWatches extends Plugin implements ThemeInterface
+{
+}
